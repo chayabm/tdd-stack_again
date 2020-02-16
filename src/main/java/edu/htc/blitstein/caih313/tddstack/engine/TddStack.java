ea33abd74faceebcdf5e6db701b5395d67655d86
chayabm <chaya.b.milstein@gmail.com>
2020-02-16 14:07:21 -0600

    Initial commit.

--- src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
+++ src/main/java/edu/htc/blitstein/caih313/tddstack/engine/TddStack.java
@@ -4,9 +4,23 @@ import edu.htc.blitstein.caih313.tddstack.IStackable;
 
 public class TddStack {
 
+    static long DEFAULT_DEPTH = 100;
+    long stackDepth;
+
+    TddStack(long stackDept){
+        stackDept = stackDepth;
+    }
+
+    public TddStack(){
+
+    }
     //to compile, this method must return something, but this
     //code is just to make it compile, not a real implementation
     boolean isEmpty(){
+        return false;
+    }
+
+    boolean isFull(){
         return false;
     }
 
